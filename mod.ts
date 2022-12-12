402398efcb3e0422e7a76aa6eb0eb8c51884bb1b
Bluemo <shutaro.aoyama@gmail.com>
2022-12-12 11:14:05 -0500

    Import scrapbox json and export markdown

--- mod.ts
+++ mod.ts
@@ -2,14 +2,26 @@ import { convertScrapboxToObsidian } from "./convert.js";
 import { parse } from "https://esm.sh/@progfay/scrapbox-parser@7.2.0"
 import { readLines } from 'https://deno.land/std/io/mod.ts'
 import clipboard from 'https://deno.land/x/clipboard/mod.ts';
+import { existsSync } from "https://deno.land/std@0.88.0/fs/exists.ts";
 
-console.log("Test Deno")
-// const lines = []
-// for await (const line of readLines(Deno.stdin)) {
-//     lines.push(line)
-// }
-const text = await clipboard.readText();
-const blocks = parse(text)
-const md = blocks.map((block) => convertScrapboxToObsidian(block, 0, "villagepump")).join("\n")
-console.log(md)
-await clipboard.writeText(md);
+if (!existsSync("./obsidianPages")) {
+    Deno.mkdir("./obsidianPages")
+}
+
+const filePath = Deno.args[0]
+try {
+    const projectFile = await Deno.readTextFile(`./${filePath}`);
+    const projectJson = JSON.parse(projectFile)
+    const pages = projectJson["pages"]
+    for (const page of pages) {
+        const blocks = parse(page["lines"].join("\n"))
+        const obsidianPage = blocks.map((block) => convertScrapboxToObsidian(block, 0, "blu3mo-public")).join("\n")
+        Deno.writeTextFile(`./obsidianPages/${page["title"].replace(/\//gi, '-')}.md`, obsidianPage);
+    }
+} catch (error) {
+    if (error instanceof Deno.errors.NotFound) {
+        console.error("the file was not found");
+    } else {
+        throw error;
+    }
+}
